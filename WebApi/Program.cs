76c71d3c389f62021378c56352fdafb9b89ed18a
merta <merta@DDP14>
2023-04-26 16:49:39 +0300

    ExceptionHandler için gerekli yapılandırma yapıldı

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using NLog;
 using Repositories.EfCore;
+using Services.Contracts;
 using WebApi.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -21,7 +22,15 @@ builder.Services.ConfigureLoggerService();
 
 var app = builder.Build();
 
+var logger = app.Services.GetRequiredService<ILoggerService>();//uygulama build edildikten sonra ihtiyacýmýz olan servisi aldýk.
+app.ConfigureExceptionHandler(logger);
 // Configure the HTTP request pipeline.
+
+if (app.Environment.IsProduction())
+{
+    app.UseHsts();
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
