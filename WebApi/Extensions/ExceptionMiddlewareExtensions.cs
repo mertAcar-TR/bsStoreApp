9d9a4cf3b3f95a76131cb6598678501d4eae0056
merta <merta@DDP14>
2023-04-26 16:43:48 +0300

    ExceptionMiddlewareExtensions kodlandı

---
+++ WebApi/Extensions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Entities.ErrorModel;
+using Microsoft.AspNetCore.Diagnostics;
+using Services.Contracts;
+using System.Net;
+
+namespace WebApi.Extensions
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static void ConfigureExceptionHandler(this WebApplication app,ILoggerService logger)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode=(int)HttpStatusCode.InternalServerError;//hangi hata olursa olsun 500 hata kodlu kabul ettik.
+                    context.Response.ContentType="application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();//hata varsa aldık
+
+                    if (contextFeature is not null)
+                    {
+                        logger.LogError($"Something went wrong : {contextFeature.Error}");
+                        await context.Response.WriteAsync(new ErrorDetails()
+                        {
+                            StatutCode= context.Response.StatusCode,
+                            Message ="Internal Server Error"
+                        }.ToString());
+                    }
+                });
+            });
+        }
+    }
+}
