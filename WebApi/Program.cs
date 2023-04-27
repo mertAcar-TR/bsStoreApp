300c09c2a9bd044bdb0a3defe325385d5776073f
merta <merta@DDP14>
2023-04-27 12:41:52 +0300

    Automapper kurulumu ve konfigürasyonu yapıldı

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -18,7 +18,7 @@ builder.Services.ConfigureSqlContext(builder.Configuration);
 builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager();
 builder.Services.ConfigureLoggerService();
-
+builder.Services.AddAutoMapper(typeof(Program));//Web API referansýný verdik.
 
 var app = builder.Build();
 
